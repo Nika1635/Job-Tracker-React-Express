c8ec2a4661c25dec4b59da4dd1eb373733f5e7a7
nika robakidze <nika70252@gmail.com>
2026-03-20 19:46:27 +0400

    post-request

--- server/router/mainRouter.js
+++ server/router/mainRouter.js
@@ -10,6 +10,6 @@ appRouter.get('/', async (req, res) => {
 
 appRouter.post("/", async (req, res) => {
     console.log(req.body)
-    const sendData = await sql `INSERT INTO jobs(company, position, status) VALUES (${req.body.company}, ${req.body.position}, ${req.body.status});`
+    const sendData = await sql `INSERT INTO jobs(company, position, status) VALUES (${req.body.company}, ${req.body.position}, "Interview");`
     res.json(sendData)
 })
