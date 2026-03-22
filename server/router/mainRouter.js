98ff5c3e0fca09045d46de03f15e90646a1261cf
nika robakidze <nika70252@gmail.com>
2026-03-22 16:38:42 +0400

    delete-method

--- server/router/mainRouter.js
+++ server/router/mainRouter.js
@@ -12,4 +12,9 @@ appRouter.post("/", async (req, res) => {
     const [ sendData ] = await sql `INSERT INTO jobs(company, position, status) VALUES (${req.body.company}, ${req.body.position}, ${req.body.status}) RETURNING *;`
     res.json(sendData)
 })
+
+appRouter.delete("/", async (req, res) => {
+    const removeData = await sql `DELETE FROM jobs WHERE id = ${req.body.id} RETURNING *;`
+    res.json(removeData)
+})
     
